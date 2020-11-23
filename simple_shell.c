4ab326b9a19110602917bd500bb8c481542be972
Michel Emad Wakim <micho.courses@gmail.com>
2020-11-23 21:18:53 +0200

    debbugging

--- simple_shell.c
+++ simple_shell.c
@@ -12,13 +12,13 @@
 //Fuction to run the shell
 void runTheShell ();
 // Function to get the current directory
-int getDirectory ();
+void getDirectory ();
 // Fuction to count the number of words in the command
 int numOfWords (char command []);
 // Function to split the command array into an array of strings
 char **splittingTheCommand (char *command, int num);
 // Function to excute the command line
-void excutingTheCommand (char ** arguments);
+void excutingTheCommand (char ** arguments, char* and);
 // Function to cahnge directory
 void changingDirectory (char* command);
 
@@ -35,56 +35,58 @@ void runTheShell () {
       // display a prompt 
 		printf("Micho.Shell ");    
 		// calling teh getDirectory function
-		getDirectory ();
+		
 		// create a string for command
 		char command[100] = {0} ; 	
 		// reading the command line	
 		scanf("%[^\n]%*c", command);  
 		/* strcmp is used to compare 2 strings in c language
 		 if strcmp returns ZERO in case of MATCHING */
-		//printf("blo%sbla\n\n",command);
-		//printf("%ld",strlen(command));
-		//break;
+		
+		if ( strlen(command) > 0) {
 		if (! strcmp(command, "exit")) {
 		   // exit the shell if the command = exit
-		   break;
+		   exit(0);
+		// checking if the command is cd 
 		} else if (command[0] == 'c' & command[1] == 'd') {
+		   // calling the function changingDirectory
 		   changingDirectory (command);
+		   // pass all the next line of codes
 		   continue;
 		}
-		 
+		// checking if the command has the & sign or not
+		char *and = strstr(command, "&"); 
 	   // Declaration of integer num that stores the number of words in the command array 
 	   int  num = numOfWords(command);
 	   // creating an array of pointers that each element in the command array is a string 
 	   char **arguments;
 	   // calling the function to split the command into an array of strings
 	   arguments = splittingTheCommand(command, num);
-	   
-	   //for (int i = 0; i< num; i++) printf("argumunts[%d] = %s\n",i,arguments[i]);
 		// calliing the excuting function
-	   excutingTheCommand (arguments);
+	   excutingTheCommand (arguments, and);
 	   // get memory free 
 		free(arguments);  
 	}
 }
+}
 
 
-int getDirectory () {
+void getDirectory () {
    // creating an array of characters to store path
-	char cwd[PATH_MAX];
+	char original_path[PATH_MAX];
 	// getting the current path
 	// after getting the path if the array cwd is not empty  
-   if (getcwd(cwd, sizeof(cwd)) != NULL) {
-       // print the path
-       printf(/*"Current working dir:*/ "-> %s $ ", cwd);
+   if (getcwd(original_path, sizeof(original_path)) != NULL) {
+      // print the path
+      //printf(/*"Current working dir:*/ "-> %s $ ", original_path);
       // if the array is empty after getting the path 
    } else {
       // print an error
       perror("getcwd() error");
       // return 1 for error
-      return 1;
+      //return 1;
    }
-   return 0;
+   //return original_path;
 }
 
 int numOfWords (char command []){
@@ -94,9 +96,10 @@ int numOfWords (char command []){
 	   by passing through all the characters in the array */
 	for(int i=0; i<strlen(command) ;i++){
 	   // if the current character is space
-	   if (command[i]==' ') 
+	   if (command[i]==' ') {
 	      // then increase the num by one
 	      num++;
+	   } 
 	}
 	//returning the value of the variable num
    return num;
@@ -105,36 +108,40 @@ int numOfWords (char command []){
 
 char **splittingTheCommand (char *command, int num) {
    // creating an array of pointers that each element in the command array is a string 
-	char **arguments = (char **)malloc(num* sizeof(char*)); 
-	int i = 1;
+	char **arguments = (char**)malloc(num * sizeof(char));
+	int i = 0;
 	//storing the first word as an element in the string
 	arguments[0] = strtok(command, " ");
-		
    //check that the spaces between the words of the command 
 	while (arguments[i] != NULL) {
 	      //storing each word as an element in the string
 			arguments[++i] = strtok(NULL," ");
-			printf("%s\n",arguments[i-1]);
    }
    return arguments;
 }
 
 
-void excutingTheCommand (char ** arguments) {
+void excutingTheCommand (char ** arguments, char* and) {
    pid_t child, parent;
 	//duplicate
-	child=fork();
+	child = fork();
 	//statu of the child;
 	int status; 
-	
+	// Zero: Returned to the newly created child process.
 	if(child == 0){
 		// execvp has its parameters as array of strings
 		execvp(arguments[0], arguments); 
 		// only get here on error
 		perror("error"); 
 		exit(1);
+	// Positive value: Returned to parent or caller. The value contains process ID of newly created child process.
 	}else if (child > 0){
-		 parent = wait(&status);
+	   // if there is the & sign in the command the parent will not wait the child process to finish  
+	   if (and == NULL) {
+	      // the parent will wait the child process to finish
+	      parent = wait(&status);
+	   }
+   // Negative Value: creation of a child process was unsuccessful.
 	}else{
 		 //error: The return of fork() is negative
 	    perror("fork failed");
@@ -144,10 +151,14 @@ void excutingTheCommand (char ** arguments) {
 }
 
 void changingDirectory (char* command) {
+   // creating a sub array path that contains the path to the change directory
    char* path =  &command[3] ;
+   // passing the path to the change directory function
    chdir(path);
    
 }
+
+
 /*(https://www.codingame.com/playgrounds/14213/how-to-play-with-strings-in-c/string-split) for splittig a string
 (https://www.usna.edu/Users/cs/aviv/classes/ic221/s16/lec/14/lec.html) the system functions */
 
