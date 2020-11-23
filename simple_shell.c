7a40c8ff9bc336b60c0b3e061a48cd212493b84f
Michel Emad Wakim <micho.courses@gmail.com>
2020-11-23 15:55:01 +0200

    Creating functions

--- simple_shell.c
+++ simple_shell.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+// include string.h to be able to use the string library in c
 #include <string.h>
 #include <unistd.h>
 // include stdlib.h to privide compatible implicit declaration of built-in function ‘exit’
@@ -9,42 +10,60 @@
 
 // Function to get the current directory
 int getDirectory ();
+// Fuction to count the number of words in the command
+int numOfWords (char command []);
+// Function to split the command array into an array of strings
+char **splittingTheCommand (char *command, int num);
 
 int main(){
 
-   printf("******************    Welcome to my simple shell    ******************\n");
+   printf("******************    Welcome to my simple shell    ******************\n\n\n");
 	while(1){
-      //display a prompt 
+      // display a prompt 
 		printf("Micho.Shell ");    
 		
+		// calling teh getDirectory function
 		getDirectory ();
 		
-		  
-		char str[100]; 		//create a string for command
-		scanf("%[^\n]%*c", str); //read the command line           
-	
-		int num=1;
-		for(int i=0; i<strlen(str) ;i++){if (str[i]==' ') num++;}//declaration the number of words in the 
-	
-		char *arg[num]; //creat an array pointer of strings //it is also an array of strings
-		int i=0;arg[0]=strtok(str, " ");//storing the first word as an element in the string
-
-		while(arg[i] != NULL){//check that the spaces between the words of the command 
-			arg[++i]= strtok(NULL," ");}//storing each word as an element in the string
+		// create a string for command
+		char command[100]; 	
+		// reading the command line	
+		scanf("%[^\n]%*c", command);  
+		
+		/* strcmp is used to compare 2 strings in c language
+		 if strcmp returns ZERO in case of MATCHING */
+		if (! strcmp(command, "exit")) {
+		   // exit the shell if the command = exit
+		   break;
+		   }
+		
+		 
+	  int  num = numOfWords(command);
+	   
+	  // creating an array of pointers that each element in the command array is a string 
+	  char **arguments;
+	  // calling the function to split the command into an array of strings
+	  arguments = splittingTheCommand(command, num);
+		
+		
 		
 		
 		pid_t child, parent;
-		child=fork();//duplicate
-		int status; //statue of the child;
+		//duplicate
+		child=fork();
+		//statu of the child;
+		int status; 
 	
-		if(child==0){
-			execvp(arg[0], arg); //execvp has its parameters as array of strings
+		if(child == 0){
+		
+			execvp(arguments[0], arguments); //execvp has its parameters as array of strings
 			perror("error"); //only get here on error
 			exit(1);}
-		else if (child>0){parent = wait(&status);}
+		else if (child > 0){parent = wait(&status);}
 		else{//error: The return of fork() is negative
 	    			perror("fork failed");_exit(2); /*exit failure, hard*/ }
 	
+	free(arguments);
 
 		
 	}
@@ -68,6 +87,37 @@ int getDirectory () {
       return 1;
    }
    return 0;
+}
+
+int numOfWords (char command []){
+   // intialinzing the integer num by value 1
+   int num = 1;
+	/* finding the number of words in the the command
+	   by passing through all the characters in the array */
+	for(int i=0; i<strlen(command) ;i++){
+	   // if the current character is space
+	   if (command[i]==' ') 
+	      // then increase the num by one
+	      num++;
+	}
+	//returning the value of the variable num
+   return num;
+}
+
+
+char **splittingTheCommand (char *command, int num) {
+   // creating an array of pointers that each element in the command array is a string 
+	char **arguments = (char **)malloc(num* sizeof(char*)); 
+	int i = 0;
+	//storing the first word as an element in the string
+	arguments[0] = strtok(command, " ");
+		
+   //check that the spaces between the words of the command 
+	while (arguments[i] != NULL) {
+	      //storing each word as an element in the string
+			arguments[++i] = strtok(NULL," ");
+   }
+   return arguments;
 }
 
 
